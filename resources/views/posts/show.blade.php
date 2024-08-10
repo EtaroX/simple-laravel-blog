057d9ec6ea1d889e900147086fd35a30d10466c2
EtaroX <95373414+EtaroX@users.noreply.github.com>
2024-08-10 21:34:01 +0200

    Feature: Added edit and delete button to specific post view if post is owned by user

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,11 +1,31 @@
 <x-app-layout>
     <x-slot name="header">
-        <div>
-            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                {{ $post->title }}
-            </h2>
-            <h3>{{$post->user->name}}</h3>
+        <div class="flex justify-between w-full">
+            <div>
+                <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                    {{ $post->title }}
+                </h2>
+                <h3>{{$post->user->name}}</h3>
+            </div>
+            <div class="flex items-center gap-0.5">
+                @if(Auth::user()->id == $post->user_id)
+                    <div>
+                        <a href="{{ route('posts.edit', $post) }}"
+                           class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Edytuj</a>
+                    </div>
+                    <div>
+                        <form action="{{ route('posts.destroy', $post) }}" method="POST">
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit"
+                                    class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">Usuń
+                            </button>
+                        </form>
+                    </div>
+                @endif
+            </div>
         </div>
+
     </x-slot>
 
     <div class="pt-5 max-w-7xl mx-auto sm:px-6 lg:px-8">
