3dc88aada3a2e4a80710063d2ab5057eb7d710d8
EtaroX <95373414+EtaroX@users.noreply.github.com>
2024-08-11 23:40:18 +0200

    Fix: Added validation of creation/edition of a post Removed: Possibility to use file (stored as base64) in post

--- resources/js/editor.js
+++ resources/js/editor.js
@@ -2,15 +2,38 @@ import Editor from '@toast-ui/editor'
 import '@toast-ui/editor/dist/toastui-editor.css';
 
 const editor = new Editor({
-  el: document.querySelector('#editor'),
-  height: '400px',
-  initialEditType: 'markdown',
+    el: document.querySelector('#editor'),
+    height: '400px',
+    initialEditType: 'markdown',
     initialValue: document.querySelector('#content').value,
-  placeholder: 'Write something cool!',
+    placeholder: 'Write something cool!',
 })
 document.querySelector('#formEdit ').addEventListener('submit', e => {
-  e.preventDefault();
-  document.querySelector('#content').value = editor.getMarkdown();
-  e.target.submit();
+    e.preventDefault();
+    if (!document.querySelector('#title').value) {
+        alert('Title is required');
+        return;
+    }
+    if (!document.querySelector('#tag').value) {
+        alert('Tag is required');
+        return;
+    }
+    document.querySelector('#content').value = editor.getMarkdown();
+    if (document.querySelector('#content').value.length < 10) {
+        alert('Content is too short');
+        return;
+    }
+    e.target.submit();
 });
 
+window.onload = () => {
+    const observer = new MutationObserver(() => {
+        if (document.querySelector('.toastui-editor-popup.toastui-editor-popup-add-image')) {
+            document.querySelector('[aria-label="URL"]').click();
+            document.querySelector('[aria-label="File"]').style.display = "none";
+        }
+    });
+
+    const target = document.querySelector(".toastui-editor-popup ");
+    observer.observe(target, {attributes: true, attributeFilter: ["style"]});
+}
